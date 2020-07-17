4adfb104d822b3695e28c2bf6f2c1a22f7ed601f
jmattmacd <47362248+jmattmacd@users.noreply.github.com>
2020-07-17 21:25:42 +0100

    Delete 09-add_impersonation_role.ps1

--- 09-add_impersonation_role.ps1
+++
@@ -1,4 +0,0 @@
-﻿# connect-exchangeonline
-$username = "GlobalAdminUser@domain.com"
-
-New-RoleGroup -Name "OnDemandMigration" -Description "Temprorary Role Group For Migration" -Roles "ApplicationImpersonation" -Members $username
