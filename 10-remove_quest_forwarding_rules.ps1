7af31b90d9ac827dd80a2b736ff61655f14b2603
jmattmacd <47362248+jmattmacd@users.noreply.github.com>
2020-07-17 21:25:49 +0100

    Delete 10-remove_quest_forwarding_rules.ps1

--- 10-remove_quest_forwarding_rules.ps1
+++
@@ -1,5 +0,0 @@
-﻿# connect-exchangeonline
-$mbxs = get-mailbox -ResultSize unlimited | where {$_.ForwardingSMTPAddress -ne $null}
-ForEach ($mbx in $mbxs) {
-    Set-Mailbox -Identity $mbx.Alias -ForwardingSmtpAddress $null -DeliverToMailboxAndForward $True
-}
