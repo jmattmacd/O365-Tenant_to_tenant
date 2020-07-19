3062442496ec30edb1d1d4af89383a4042843ed7
jmattmacd <47362248+jmattmacd@users.noreply.github.com>
2020-07-19 15:45:16 +0100

    Update 01-users.ps1

--- 01-users.ps1
+++ 01-users.ps1
@@ -1,6 +1,6 @@
 ﻿# Connect-AzureAD
-$DomainName1 = "*@arriva.com.hr"
-$DomainName2 = "*@arriva.fr"
+$DomainName1 = "*@thisdomain.com"
+$DomainName2 = "*@thatdomain.com"
 $Scopedusers = Get-AzureADUser -all $true | where {$_.userprincipalname -like $DomainName1} | select userprincipalname, GivenName, Surname, DisplayName, mailNickName
 $Scopedusers+= Get-AzureADUser -all $true | where {$_.userprincipalname -like $DomainName2} | select userprincipalname, GivenName, Surname, DisplayName, mailNickName
 $Scopedusers | export-csv -Path ".\01-output-inscope_users.csv"
